263216a4dc7200cc04c379584989f9887decceca
Xavier Leune <xleune@commentcamarche.net>
2015-01-28 10:03:07 +0100

    TingUserBundle/Model/User now implements EquatableInterface to check security on every request

--- src/TingUserBundle/Model/User.php
+++ src/TingUserBundle/Model/User.php
@@ -28,8 +28,10 @@ namespace CCMBenchmark\TingUserBundle\Model;
 use CCMBenchmark\Ting\Entity\NotifyProperty;
 use CCMBenchmark\Ting\Entity\NotifyPropertyInterface;
 use FOS\UserBundle\Model\GroupInterface;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class User extends \FOS\UserBundle\Model\User implements NotifyPropertyInterface
+class User extends \FOS\UserBundle\Model\User implements NotifyPropertyInterface, EquatableInterface
 {
     use NotifyProperty;
 
@@ -226,5 +228,37 @@ class User extends \FOS\UserBundle\Model\User implements NotifyPropertyInterface
     public function getCredentialsExpireAt()
     {
         return $this->credentialsExpireAt;
+    }
+
+    /**
+     * Implementation of EquatableInterface
+     *
+     * The equality comparison should neither be done by referential equality
+     * nor by comparing identities (i.e. getId() === getId()).
+     *
+     * However, you do not need to compare every attribute, but only those that
+     * are relevant for assessing whether re-authentication is required.
+     *
+     * Also implementation should consider that $user instance may implement
+     * the extended user interface `AdvancedUserInterface`.
+     *
+     * @param UserInterface $user
+     *
+     * @return bool
+     */
+    public function isEqualTo(UserInterface $user)
+    {
+        if ($user instanceof User) {
+            // Check that the roles are the same, in any order
+            $isEqual = count($this->getRoles()) == count($user->getRoles());
+            if ($isEqual) {
+                foreach ($this->getRoles() as $role) {
+                    $isEqual = $isEqual && in_array($role, $user->getRoles());
+                }
+            }
+            return $isEqual;
+        }
+
+        return false;
     }
 }
